b10a15909752e3463d03ebcbef7571762b1550e8
MatthewEnamuotor <90224141+oviematthew@users.noreply.github.com>
2023-08-21 00:32:27 -0400

    files fixed

--- php/mail.php
+++ php/mail.php
@@ -1,31 +1,41 @@
 <?php
-$subject = 'OVIEMATTHEW CONTACT FORM'; // Subject of your email
-$to = 'ovieenams@gmail.com';  //Recipient's E-mail
+// Define your custom email address
+$customFromEmail = 'form@oviematthew.com';
 
+// Define your email settings
+$subject = 'OVIEMATTHEW CONTACT FORM';
+$to = 'ovieenams@gmail.com';
 
-$name = $_REQUEST['name'];
-$email = $_REQUEST['email'];
-$msg = $_REQUEST['msg'];
+// Sanitize user inputs
+$name = htmlspecialchars($_POST['name'], ENT_QUOTES, 'UTF-8');
+$email = filter_var($_POST['email'], FILTER_VALIDATE_EMAIL);
+$msg = htmlspecialchars($_POST['msg'], ENT_QUOTES, 'UTF-8');
 
-
-$email_from = $name.'<'.$email.'>';
-
-
-$message .= 'Name : ' . $name . "\n";
-$message .= 'Email Address : ' . $email . "\n";
-$message .= 'Message : ' . $msg . "\n";
-
-
-
-if (@mail($to, $subject, $message, $email_from))
-{
-	// Transfer the value 'sent' to ajax function for showing success message.
-	header("Location: ../success.html");
-    
+// Validate email
+if (!$email) {
+    header("Location: ../failed.html");
+    exit();
 }
-else
-{
-	// Transfer the value 'failed' to ajax function for showing error message.
-	header("Location: ../failed.html");
+
+// Construct the message
+$message = "Name: $name\n";
+$message .= "Email Address: $email\n";
+$message .= "Message: $msg\n";
+
+// Create email headers
+$headers = "From: $customFromEmail" . "\r\n";
+$headers .= "Reply-To: $email" . "\r\n";
+$headers .= "MIME-Version: 1.0" . "\r\n";
+$headers .= "Content-type: text/plain; charset=UTF-8" . "\r\n";
+
+// Send the email
+$mailSuccess = mail($to, $subject, $message, $headers);
+
+if ($mailSuccess) {
+    // Redirect to success page
+    header("Location: ../success.html");
+} else {
+    // Redirect to failure page
+    header("Location: ../failed.html");
 }
 ?>
