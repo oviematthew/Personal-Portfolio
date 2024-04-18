f92f4493aad5f14406ddcec31e6f4ade169b6c65
MatthewEnamuotor <90224141+oviematthew@users.noreply.github.com>
2024-04-18 16:09:20 -0400

    updated portfolio

--- js/index.js
+++ js/index.js
@@ -223,3 +223,55 @@ $("body")
         }
       );
   });
+
+//ting App
+$("body")
+  .on("click", "#tingLaunch", function () {
+    var tingVideoId = $(this).data("vimeo-id"),
+      $video =
+        '<div id="vimeo-pop-container"><div><span class="vimeo-close"></span><iframe src="https://player.vimeo.com/video/936424594?badge=0&amp;autopause=0&amp;player_id=0&amp;app_id=58479" frameborder="0" allow="autoplay; fullscreen; picture-in-picture" style="position:absolute;top:0;left:0;width:100%;height:100%;" title="todo-app-demo"></iframe></div></div>';
+    $("body").append($video);
+  })
+
+  .on("click", ".vimeo-close", function () {
+    $(this).addClass("closing");
+
+    $("#vimeo-pop-container")
+      .delay(700)
+      .animate(
+        {
+          height: 0,
+          top: "50%",
+        },
+        "fast",
+        function () {
+          $("#vimeo-pop-container").remove();
+        }
+      );
+  });
+
+//chatee App
+$("body")
+  .on("click", "#chateeLaunch", function () {
+    var chateeVideoId = $(this).data("vimeo-id"),
+      $video =
+        '<div id="vimeo-pop-container"><div><span class="vimeo-close"></span><iframe src="https://player.vimeo.com/video/936426315?badge=0&amp;autopause=0&amp;player_id=0&amp;app_id=58479" frameborder="0" allow="autoplay; fullscreen; picture-in-picture" style="position:absolute;top:0;left:0;width:100%;height:100%;" title="todo-app-demo"></iframe></div></div>';
+    $("body").append($video);
+  })
+
+  .on("click", ".vimeo-close", function () {
+    $(this).addClass("closing");
+
+    $("#vimeo-pop-container")
+      .delay(700)
+      .animate(
+        {
+          height: 0,
+          top: "50%",
+        },
+        "fast",
+        function () {
+          $("#vimeo-pop-container").remove();
+        }
+      );
+  });
